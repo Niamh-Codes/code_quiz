b42771bb9d238173447e2ec596df058595a1a13d
avocado.codes <151567906+Niamh-Codes@users.noreply.github.com>
2024-01-12 16:31:59 +0000

    testing timer function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,2 +1,17 @@
 //starter
 let start = document.querySelector('#start');
+var timerCount = document.querySelector("#timer")
+
+var secondsLeft = 100
+
+//Timer countdown
+// let timerCount = document.querySelector(".timer")
+function timer (){
+    start.addEventListener('click', function() {
+        timerCount = setInterval(function() {
+            console.log('!')
+        }, 1000);
+    });
+}
+
+
