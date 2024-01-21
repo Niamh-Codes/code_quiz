8c5f3c7964fd8a0728a868cdf2e0f6432e3382b3
avocado.codes <151567906+Niamh-Codes@users.noreply.github.com>
2024-01-21 15:56:58 +0000

    start function of quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,12 +4,12 @@ const nextButton = document.getElementById('next');
 const submitButton = document.getElementById('submit');
 const timerElement = document.getElementById('time');
 const questionTitleElement = document.getElementById('question-title');
-const choicesElement = document.getElementById('choices');
+const choices = document.getElementById('choices');
 const finalScoreElement = document.getElementById('final-score');
 const initialsInput = document.getElementById('initials');
 
 startButton.addEventListener('click', startQuiz);
-nextButton.addEventListener('click', loadNextQuestion);
+nextButton.addEventListener('click', loadQuestion);
 submitButton.addEventListener('click', submitQuiz);
 
 
@@ -21,8 +21,30 @@ let timeLeft = 50;
 function startQuiz() {
   document.getElementById('start-screen').classList.add('hide');
   document.getElementById('questions').classList.remove('hide');
+  document.getElementById('next').classList.remove('hide');
 
- 
+ // Start time
+ startTimer();
+
+ // Fiirst question
+ loadQuestions();
+}
+
+function startTimer() {
+ const timerInterval = setInterval(function () {
+   timeLeft--;
+
+   if (timeLeft <= 0) {
+     clearInterval(timerInterval);
+     endQuiz();
+   }
+
+   timerElement.textContent = `: ${timeLeft}`;
+ }, 1000);
+}
+
+
+
 
 
 
